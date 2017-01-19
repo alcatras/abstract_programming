5035b9b85d3904776a6fe04f3f3ec9ae0caa7367
alcatras <klimalakamil@gmail.com>
2017-01-19 18:08:47 +0100

    Resolved merge conflicts.

---
+++ src/command_line/Query.h
@@ -0,0 +1,15 @@
+//
+// Created by kamil on 17.01.17.
+//
+
+#ifndef ABSTRACT_PROGRAMMING_QUERY_H
+#define ABSTRACT_PROGRAMMING_QUERY_H
+
+#include <string>
+
+class Query {
+
+};
+
+
+#endif //ABSTRACT_PROGRAMMING_QUERY_H
